656aca2710122d5d703a2903ef1339787a48b284
Brandon Pitts <brandomoki.bp@gmail.com>
2022-08-19 23:24:39 -0700

    Added all new info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,27 +31,12 @@ class Footer extends Component {
               >
                 <span
                   className="iconify language-icon mr-5"
-                  data-icon="twemoji-flag-for-flag-united-kingdom"
+                  data-icon="twemoji-flag-for-flag-united-states"
                   data-inline="false"
                   id={window.$primaryLanguageIconId}
                 ></span>
               </div>
-              <div 
-                onClick={() =>
-                  this.props.applyPickedLanguage(
-                    window.$secondaryLanguage,
-                    window.$primaryLanguageIconId
-                  )
-                }
-                style={{ display: "inline" }}
-              >
-                <span
-                  className="iconify language-icon"
-                  data-icon="twemoji-flag-for-flag-poland"
-                  data-inline="false"
-                  id={window.$secondaryLanguageIconId}
-                ></span>
-              </div>
+              
             </div>
             <div className="col">
               <div className="copyright py-4 text-center">
